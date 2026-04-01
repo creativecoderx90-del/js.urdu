df6106765969d3739289da6440daaef2979527d2
creativecoderx90-del <creativecoderx90@gmail.com>
2026-04-01 07:45:49 +0000

    dataTypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,10 @@
+// let const and var
+const accountId=144553;
+let accountEmail="devgirl.com"
+// prefer not to use var because of issue in block scope and functional scope
+var accountPassword="65758797"
+accountCity="gujranwala"
+// accountId=2
+accountPassword="113344"
+console.log(accountPassword);
+console.table([accountId, accountEmail, accountPassword, accountCity])//we can use console.table for printing different things at one time
---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,20 @@
+ "use strict";//treats all js code as newer version
+//  alert("3+3") we are using node.js not browser
+console.log(3+3);//code readability should be high
+console.log("devgirl")
+let name="devgirl"
+let age=19
+let isLoggedIn=false
+// number=>2 to power 53
+// bigInt=>for large numbers
+// string =>"assembly of characters"
+// boolean=>true/false
+// null =>stand alone value (type=object)
+// undefined =>kindda placeholder for a value which is not defined yet
+// symbol =>unique
+// object 
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof undefined)
+console.log(typeof null)
